6341bcb92579f9f2c8938cdc9679a53b30abedae
Prejith P <prejith.p@innovinlabs.com>
2025-04-01 17:22:00 +0530

    Initial 4

--- app/profile/[nickname]/page.tsx
+++ app/profile/[nickname]/page.tsx
@@ -1,13 +1,14 @@
 import Navbar from "@/components/Navbar";
 import ProfileContent from "./ProfileContent";
 
-interface PageProps {
+type Props = {
   params: {
     nickname: string;
   };
-}
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
-export default function ProfilePage({ params }: PageProps) {
+export default function ProfilePage({ params }: Props) {
   return (
     <>
       <Navbar />
