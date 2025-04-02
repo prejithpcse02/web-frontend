4fa2f15aa59ad1ec0c84ca83eb1850c504db7c9c
Prejith P <prejith.p@innovinlabs.com>
2025-04-02 10:04:36 +0530

    Initial Commit 11

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "127.0.0.1",
-      "localhost",
-      "https://web-frontend-w5a4.vercel.app/",
-    ],
+    domains: ["127.0.0.1", "localhost", "web-frontend-w5a5.vercel.app"],
     remotePatterns: [
       {
         protocol: "http",
