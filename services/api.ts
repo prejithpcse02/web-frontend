6e017229f8b9517e3a84754592b235f1aae1ce2c
Prejith P <prejith.p@innovinlabs.com>
2025-04-02 10:21:56 +0530

    Initial Commit 11

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // Create an axios instance with default config
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+const API_BASE_URL = "http://127.0.0.1:8000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
